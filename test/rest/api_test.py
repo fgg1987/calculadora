a3c9101a1fdc8b5715d8d79427e683677ed657fd
fgg1987 <134975346+fgg1987@users.noreply.github.com>
2023-12-11 17:21:41 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -7,7 +7,7 @@ import pytest
 
 BASE_URL = "http://localhost:5000"
 BASE_URL_MOCK = "http://localhost:9090"
-DEFAULT_TIMEOUT = 60  # in secs
+DEFAULT_TIMEOUT = 120  # in secs
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
@@ -25,6 +25,26 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/2/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "6", "ERROR MULTIPLY"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/4/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "2", "ERROR DIVIDE"
+        )
+        
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
